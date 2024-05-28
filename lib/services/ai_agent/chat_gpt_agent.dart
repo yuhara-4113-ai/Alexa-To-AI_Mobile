53e06727c5c051dc097a1f3891fce37dcfcc488b
yuhara-4113-ai <theme.of.es.i@gmail.com>
2024-05-28 20:17:45 +0900

    ChatGPT Review 変数名の誤字を修正 入力メッセージ処理の簡潔化 コメント内容の修正および整理

--- lib/services/ai_agent/chat_gpt_agent.dart
+++ lib/services/ai_agent/chat_gpt_agent.dart
@@ -9,40 +9,34 @@ class ChatGPTAgent implements AIAgent {
     setOpenAIClient(aiModel.apiKey);
 
     final userMessage = OpenAIChatCompletionChoiceMessageModel(
-      content: [
-        OpenAIChatCompletionChoiceMessageContentItemModel.text(prompt),
-      ],
+      content: [OpenAIChatCompletionChoiceMessageContentItemModel.text(prompt)],
       role: OpenAIChatMessageRole.user,
     );
 
-    // これまでのやり取りを設定(今は単発)
+    // これまでのやり取りを設定 (今は単発)
     final requestMessages = [
-      // systemMessage,
       userMessage,
     ];
 
     // OpenAIにリクエスト
-    final respnse = await OpenAI.instance.chat.create(
+    final response = await OpenAI.instance.chat.create(
       model: aiModel.model,
       messages: requestMessages,
       // TODO max_tokensはとりあえず固定値。後で設定画面から変更できるようにする
       maxTokens: 500,
-      // AI回答のランダム性を持たせる場合は何かしらの数値を設定
-      // AIの出力に一貫性を持たせるために使用される。seedを設定すると、AIは同じseed値を使用して同じプロンプトに対して同じレスポンスを生成します。
-      // seedを設定しない場合や異なるseed値を使用すると、AIのレスポンスは異なる可能性がある。
-      // TODO 設定画面から変更できるようにする
-      seed: 6,
-      // 生成されるテキストのランダム性と多様性を制御するためのパラメーター
-      // temperatureは0から1までの値を取り、高い値はよりランダムなテキストを、低い値はより決定論的（予測可能）なテキストを生成します。
-      // 具体的には、temperatureが1に近いと、AIは多様なレスポンスを生成し、それぞれの単語の選択肢をほぼ等しく考慮します。一方、temperatureが0に近いと、AIは最も確率的に高い単語を選択し、出力はより一貫性があります。
-      // したがって、temperatureを調整することで、AIの出力の創造性と予測可能性のバランスを制御することができます。
-      // TODO 設定画面から変更できるようにする
+      // 生成されるテキストのランダム性と多様性を制御
+      // temperatureが1に近いと、AIは多様なレスポンスを生成し、それぞれの単語の選択肢をほぼ等しく考慮します。
+      // 一方、temperatureが0に近いと、AIは最も確率的に高い単語を選択し、出力はより一貫性があります。
+      // TODO とりあえず固定値。設定画面から変更できるようにする
       temperature: 0.2,
+      // seedを設定してレスポンスの一貫性を保持（必要性があれば利用）
+      seed: 6,
     );
 
     // TODO 入出力のtoken(total_tokens)を保持し、このアプリでどのくらいのtoken数(料金)を消費しているかを表示したい
-    debugPrint('totalTokens: ${respnse.usage.totalTokens.toString()}');
-    String responseText = respnse.choices.first.message.content![0].text!;
+    debugPrint('totalTokens: ${response.usage.totalTokens.toString()}');
+
+    String responseText = response.choices.first.message.content![0].text!;
     debugPrint('responseText: $responseText');
     return responseText;
   }
