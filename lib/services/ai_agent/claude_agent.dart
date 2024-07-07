80499c1efcf8c6e3bb8c562346601c392599aa2e
yuhara-4113-ai <theme.of.es.i@gmail.com>
2024-07-07 21:26:04 +0900

    35-claude-package-replacement ・速度改善 速度が3倍ほど早くなった。 パッケージではなく自前で実装した処理を使用

--- lib/services/ai_agent/claude_agent.dart
+++ lib/services/ai_agent/claude_agent.dart
@@ -1,40 +1,49 @@
+import 'dart:convert';
 import 'package:logger/logger.dart';
+import 'package:http/http.dart' as http;
 
 import 'package:alexa_to_ai/models/ai_model.dart';
 import 'package:alexa_to_ai/services/ai_agent/ai_agent.dart';
-import 'package:anthropic_dart/anthropic_dart.dart';
 
 final log = Logger();
+final url = Uri.https('api.anthropic.com', '/v1/messages');
 
 class ClaudeAgent implements AIAgent {
   @override
   Future<String> sendMessage(String prompt, AIModel aiModel) async {
-    final AnthropicService service =
-        AnthropicService(aiModel.apiKey, model: aiModel.model);
+    final headers = {
+      'Content-Type': 'application/json',
+      'X-API-Key': aiModel.apiKey,
+      'Anthropic-Version': '2023-06-01',
+    };
 
-    // AIリクエストの作成
-    final Request aiRequest = _createAIRequest(prompt);
+    final body = jsonEncode({
+      'model': aiModel.model,
+      'max_tokens': 500,
+      'system': "",
+      'messages': [
+        {'role': 'user', 'content': prompt},
+      ]
+    });
 
-    try {
-      final Response response = await service.sendRequest(request: aiRequest);
-      final String responseText = response.toJson()["content"][0]["text"];
-      log.i('Response body: $responseText');
+    final response = await http.post(
+      url,
+      headers: headers,
+      body: body,
+    );
 
-      return responseText;
-    } catch (e) {
-      log.i('Error while sending request: $e');
+    if (response.statusCode == 200) {
+      try {
+        final Map<String, dynamic> responseData =
+            jsonDecode(utf8.decode(response.bodyBytes));
+        return responseData['content'][0]['text'];
+      } catch (e) {
+        log.e('Error decoding response: $e');
+        throw Exception("AIリクエストの送信中にエラーが発生しました");
+      }
+    } else {
+      log.e('Error response: $response');
       throw Exception("AIリクエストの送信中にエラーが発生しました");
     }
-  }
-
-  Request _createAIRequest(String prompt) {
-    return Request()
-      ..maxTokens = 500
-      ..messages = [
-        Message(
-          role: "user",
-          content: prompt,
-        )
-      ];
   }
 }
