4463fddeec56c666a559a1687978bf6fa095d2e6
yuhara-4113-ai <theme.of.es.i@gmail.com>
2024-11-20 18:24:00 +0900

    46-support-for-chat-gpt-o1-model ・ChatGPT o1系モデルを使用できるように対応 ・ついでにGeminiの新モデルのgemini-exp-1114を追加 ・ついでにライブラリのバージョンも更新

--- lib/services/ai_agent/chat_gpt_agent.dart
+++ lib/services/ai_agent/chat_gpt_agent.dart
@@ -1,53 +1,44 @@
 import 'package:logger/logger.dart';
+import 'package:openai_dart/openai_dart.dart';
 
 import 'package:alexa_to_ai/models/ai_model.dart';
 import 'package:alexa_to_ai/services/ai_agent/ai_agent.dart';
-import 'package:dart_openai/dart_openai.dart';
 
 final log = Logger();
 
 class ChatGPTAgent implements AIAgent {
   @override
-  Future<String> sendMessage(String prompt, AIModel aiModel) async {
-    setOpenAIClient(aiModel.apiKey);
-
-    final userMessage = OpenAIChatCompletionChoiceMessageModel(
-      content: [OpenAIChatCompletionChoiceMessageContentItemModel.text(prompt)],
-      role: OpenAIChatMessageRole.user,
-    );
-
-    // これまでのやり取りを設定 (今は単発)
-    final requestMessages = [
-      userMessage,
-    ];
+  Future<String> sendMessage(final String prompt, AIModel aiModel) async {
+    final client = OpenAIClient(apiKey: aiModel.apiKey);
 
     // OpenAIにリクエスト
-    final response = await OpenAI.instance.chat.create(
-      model: aiModel.model,
-      messages: requestMessages,
-      // TODO max_tokensはとりあえず固定値。後で設定画面から変更できるようにする
-      maxTokens: 500,
-      // 生成されるテキストのランダム性と多様性を制御
-      // temperatureが1に近いと、AIは多様なレスポンスを生成し、それぞれの単語の選択肢をほぼ等しく考慮します。
-      // 一方、temperatureが0に近いと、AIは最も確率的に高い単語を選択し、出力はより一貫性があります。
-      // TODO とりあえず固定値。設定画面から変更できるようにする
-      temperature: 0.2,
-      // seedを設定してレスポンスの一貫性を保持（必要性があれば利用）
-      seed: 6,
+    final response = await client.createChatCompletion(
+      request: CreateChatCompletionRequest(
+        model: ChatCompletionModel.modelId(aiModel.model),
+        messages: [
+          ChatCompletionMessage.user(
+            content: ChatCompletionUserMessageContent.string(prompt),
+          ),
+        ],
+        // TODO maxCompletionTokensはとりあえず固定値。後で設定画面から変更できるようにする
+        // 以下の場合はすいろんするため、通常のtokenより消費量があがる
+        // 通常：160
+        // o1-mini：500
+        // o1-preview：1600
+        maxCompletionTokens: 1000,
+        // seedを設定してレスポンスの一貫性を保持（必要性があれば利用）
+        seed: 6,
+      ),
     );
+    // ライブラリのExampleに書いてあったので一応endSessionしておく
+    client.endSession();
 
     // TODO 入出力のtoken(total_tokens)を保持し、このアプリでどのくらいのtoken数(料金)を消費しているかを表示したい
-    log.i('totalTokens: ${response.usage.totalTokens.toString()}');
+    log.i('totalTokens: ${response.usage!.totalTokens.toString()}');
 
-    String responseText = response.choices.first.message.content![0].text!;
+    String responseText = response.choices.first.message.content!;
     log.i('responseText: $responseText');
-    return responseText;
-  }
 
-  void setOpenAIClient(String apiKey) {
-    OpenAI.apiKey = apiKey;
-    OpenAI.requestsTimeOut = const Duration(seconds: 60);
-    // パッケージの操作ログを出力
-    OpenAI.showLogs = true;
+    return responseText;
   }
 }
